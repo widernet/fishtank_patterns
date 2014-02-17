db02ed56add8137f821c5a5a3e9010eeaefee20a
Dom Christie <christiedom@gmail.com>
2014-02-17 17:42:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FishtankPatterns::Application.config.secret_key_base = 'e8d608e946c40ef174521e9ac06eb01420767f6dcff02cfb3f5a36ea22d69deff9787c5e8a568a968f011a4465c060bf50e298e6827e7b6db68f0ac7c6c113d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FishtankPatterns::Application.config.session_store :cookie_store, key: '_fishtank_patterns_session'
